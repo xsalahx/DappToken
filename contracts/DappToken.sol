7d6a57fd5ef3269d22592b0e1bb04471a1b8d9d3
salah <salahyo99@gmail.com>
2020-09-05 14:08:42 +0100

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,7 +12,15 @@ contract DappToken {
 		uint256 _value
 	);
 
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
+
+
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
 
 	constructor(uint256 _initialSupply) public {
@@ -29,4 +37,24 @@ contract DappToken {
 		emit Transfer(msg.sender,_to,_value);
 		return true;
 	}
+
+	function approve(address _spender, uint256 _value) public returns(bool success) {
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender,_spender,_value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+		require(balanceOf[_from] >= _value);
+		require(allowance[_from][msg.sender] >= _value);
+
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		allowance[_from][msg.sender] -= _value;
+
+		emit Transfer(_from,_to,_value);
+		return true;
+	}
+
 }
